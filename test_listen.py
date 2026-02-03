ba3998cebd27c8dde26e38a2bab46e2afc7033be
DileepKrishnaM <dileepkrishna.madineni@gmail.com>
2026-02-03 21:07:42 +0530

    delete: listen, speak testing files

--- test_listen.py
+++
@@ -1,5 +0,0 @@
-from core.listener import listen
-
-print("Say something...")
-text = listen()
-print(f"You said: {text}")
--- test_voice.py
+++
@@ -1,2 +0,0 @@
-from core.speaker import speak
-speak("Donna is online. All systems up and running.")
